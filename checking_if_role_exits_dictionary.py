e8ed163a50a5f19903ef1d0359d2fc85b68e401d
muhammedmurad-cloud <muradovmehemmed803@gmail.com>
2026-07-27 21:38:45 +0400

    Add files via upload

---
+++ checking_if_role_exits_dictionary.py
@@ -0,0 +1,4 @@
+roles = {"Alice" : "Admin","Bob" : "Editor", "Muhammed" : "Data Analytic", "Carol" : "Viewer"}
+print("Editor exists :","Editor" in roles.values())
+print("Manager exists :","Manager" in roles.values())
+#Checking if the role exists in company
