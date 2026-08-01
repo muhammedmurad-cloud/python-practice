6de56382e824bc3a7ae30ea1e735535c41a2aef2
muhammedmurad-cloud <muradovmehemmed803@gmail.com>
2026-08-02 01:02:55 +0400

    Delete fav_movies.py

--- fav_movies.py
+++
@@ -1,7 +0,0 @@
-fav_movies = ["Interstellar","Inception","Zodiac"]
-file_path = "output.txt"
-with open(file_path,"w") as file:
-    for movie in fav_movies:
-        file.write(movie + "\n")
-    print(f"txt file {file_path} was created")
-    
